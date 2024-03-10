bb0a45bbfbc2837c4804f47e280cc5f567dc87ed
elvin1990huseynlii <161587697+elvin1990huseynlii@users.noreply.github.com>
2024-03-11 01:09:19 +0400

    Add files via upload

---
+++ lesson.py
@@ -0,0 +1,160 @@
+
+# 1 - Pərakəndə:
+# Məhsulun adı və qiyməti üçün istifadəçidən məlumat alan, sonra detalları formatlaşdırılmış şəkildə çap edən Python proqramı yazın. Proqram 6 dəfə məlumat almalıdır.
+
+
+
+# product = []
+
+# for _ in range(6):
+#     name = (input('Məhsulun adını daxil edin: '))
+#     price = (input('Məhsulun qiymətini daxil edin: '))
+#     userinput = [name, price]
+#     product.append(userinput)
+
+# print(product)
+
+
+# 3 - Səhiyyə:
+# Xəstə adı və yaşı üçün istifadəçidən məlumat alan, sonra detalları formatlaşdırılmış şəkildə çap edən Python proqramı yazın. Proqram 5 dəfə məlumat almalıdır.
+
+
+# patient = []
+
+# for _ in range(5):
+#     name = (input('Xəstənin adını daxil edin: '))
+#     age = (input('Xəstənin yaşını daxil edin: '))
+#     userinput = [name, age]
+#     patient.append(userinput)
+
+# print(patient)
+
+
+
+# 4 - Təhsil :
+# Tələbə adı və yaşı üçün istifadəçidən məlumat alan, sonra detalları formatlaşdırılmış şəkildə çap edən Python proqramı yazın. Proqram 8 dəfə məlumat almalıdır.
+
+
+# student = []
+
+# for _ in range(8):
+#     name = (input('Tələbə adını daxil edin: '))
+#     age = (input('Tələbə yaşını daxil edin: '))
+#     userinput = [name, age]
+#     student.append(userinput)
+
+# print(student)
+
+
+# 5 - Restoran:
+# Qida məhsullarının adı və qiyməti üçün istifadəçidən məlumat alan, sonra detalları formatlaşdırılmış şəkildə çap edən Python proqramı yazın. 
+# Proqram 4 dəfə məlumat almalıdır.
+
+
+# product = []
+
+# for _ in range(4):
+#     name = (input('Qida məhsulunun adını daxil edin: '))
+#     price = (input('Məhsulun qiymətini daxil edin: '))
+#     userinput = [name, price]
+#     product.append(userinput)
+
+# print(product)
+
+
+# 6 - Texnologiya:
+# Proqram tərtibatçılarının adı, yaşı və istifadə etdiyi proqram dili üçün istifadəçidən məlumat alan, 
+# sonra detalları formatlaşdırılmış şəkildə çap edən Python proqramı yazın. 
+# Proqram 4 dəfə məlumat almalıdır.
+
+
+# programming = []
+
+# for _ in range(4):
+#     name = (input('Adınızı daxil edin: '))
+#     age = (input('Yaşınızı daxil edin: '))
+#     language = (input('İstifadə etdiyiniz dili daxil edin: '))
+#     userinput = [name, age, language]
+#     programming.append(userinput)
+
+# print(programming)
+
+
+
+# 8 - Əyləncə:
+# Filmlərin adı və istehsal tarixi üçün istifadəçidən məlumat alan, sonra detalları formatlaşdırılmış şəkildə çap edən
+# Python proqramı yazın. Proqram 4 dəfə məlumat almalıdır.
+
+
+# films = []
+
+# for _ in range(4):
+#     name = (input('Filmin adını daxil edin: '))
+#     date = (input('İstehsal tarixini daxil edin: '))
+#     userinput = [name, date]
+#     films.append(userinput)
+
+# print(films)
+
+
+# 9 - Daşınmaz Əmlak:
+# Evin kvadratı sahəsi və qiyməti üçün istifadəçidən məlumat alan, sonra detalları formatlaşdırılmış şəkildə çap edən Python proqramı yazın. Proqram 4 dəfə məlumat almalıdır.
+
+# home = []
+
+# for _ in range(4):
+#     square = (input('Evin kvadratını daxil edin: '))
+#     price = (input('Qiymetini daxil edin: '))
+#     userinput = [square, price]
+#     home.append(userinput)
+
+# print(home)
+
+
+# 10 - Elektron ticarət:
+# Məhsulun adı və qiyməti üçün istifadəçidən məlumat alan, sonra detalları formatlaşdırılmış şəkildə çap edən 
+# Python proqramı yazın. Proqram 7 dəfə məlumat almalıdır.
+
+# product = []
+
+# for _ in range(7):
+#     name = (input('Məhsulun adını daxil edin: '))
+#     price = (input('Qiymetini daxil edin: '))
+#     userinput = [name, price]
+#     product.append(userinput)
+
+# print(product)
+
+
+
+# 11 - Kənd Təsərrüfatı:
+# Tərəvəzin adı və qiyməti üçün istifadəçidən məlumat alan, sonra detalları formatlaşdırılmış şəkildə çap edən 
+# Python proqramı yazın. 
+# Proqram 2 dəfə məlumat almalıdır.
+
+# vegetables = []
+
+# for _ in range(2):
+#     name = (input('Tərəvəzin adını daxil edin: '))
+#     price = (input('Qiymetini daxil edin: '))
+#     userinput = [name, price]
+#     vegetables.append(userinput)
+
+# print(vegetables)
+
+
+
+# 12 - Telekommunikasiya:
+# Telefonun adı və qiyməti üçün istifadəçidən məlumat alan, 
+# sonra detalları formatlaşdırılmış şəkildə çap edən Python proqramı yazın. 
+# Proqram 6 dəfə məlumat almalıdır.
+
+# smartphone = []
+
+# for _ in range(6):
+#     name = (input('Telefonun adını daxil edin: '))
+#     price = (input('Qiymetini daxil edin: '))
+#     userinput = [name, price]
+#     smartphone.append(userinput)
+
+# print(smartphone)
